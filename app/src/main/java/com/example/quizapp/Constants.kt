8e86d1392bb4220177cd0bb465a28bbf4b97bd8a
imshukla10 <ritesh.1923ec1099@kiet.edu>
2021-12-06 18:54:53 +0530

    Completed

--- app/src/main/java/com/example/quizapp/Constants.kt
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -1,6 +1,11 @@
 package com.example.quizapp
 
 object Constants {
+
+    const val USER_NAME:String="user_name"
+    const val TOTAL_QUESTIONS:String = "total_question"
+    const val CORRECT_ANSWERS:String = "correct_answers"
+
     fun getQuestion():ArrayList<Question>{
         val questionList = ArrayList<Question>()
         val ques1 = Question(1,
@@ -10,7 +15,7 @@ object Constants {
             "Argentina",
             "Australia",
             "Austria",
-            1
+            2
         )
         questionList.add(ques1)
 
